f27e98a3977f404eedaac7fcf8832cd63cb49053
JaeyoungLim <gokoreas@gmail.com>
2020-04-20 00:25:30 +0200

    Add test

--- geometric_controller/test/geometric_controller-test.cpp
+++ geometric_controller/test/geometric_controller-test.cpp
@@ -30,3 +30,18 @@ TEST(GeometricControllerTest, acc2quaternion) {
   
   ASSERT_TRUE(attitude.isApprox(ref_attitude));
 }
+
+TEST(GeometricControllerTest, matrix_hat) {
+  Eigen::Vector3d test;
+  Eigen::Vector3d result;
+  Eigen::Matrix3d matrix;
+  test<< 1.0, 2.0, 3.0;
+
+  result = geometricCtrl::matrix_hat_inv(geometricCtrl::matrix_hat(test));
+
+  // ASSERT_TRUE(result.isApprox(test));
+  EXPECT_EQ(result(7), test(0));
+  EXPECT_EQ(result(2), test(1));
+  EXPECT_EQ(result(3), test(2));
+
+}
